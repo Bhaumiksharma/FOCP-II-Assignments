4b3b4f0a3c44ab17ccc8593fec5c3e34882aa486
bhaumik25csu047-ctrl <bhaumik25csu047@ncuindia.edu>
2026-04-26 21:05:36 +0530

    bank_balance

--- bank_balance.cpp
+++ bank_balance.cpp
@@ -3,41 +3,54 @@ using namespace std;
 
 class Bank {
     unordered_map<string, long long> mp;
-
 public:
-    bool CREATE(string x, long long y) {
-        if (!mp.count(x)) return mp[x] = y, true;
-        mp[x] += y; return false;
+    bool CREATE(string X, long long Y) {
+        auto it = mp.find(X);
+        if (it == mp.end()) { mp[X] = Y; return true; }
+        it->second += Y;
+        return false;
     }
 
-    bool DEBIT(string x, long long y) {
-        if (!mp.count(x) || mp[x] < y) return false;
-        mp[x] -= y; return true;
+    bool DEBIT(string X, long long Y) {
+        auto it = mp.find(X);
+        if (it == mp.end() || it->second < Y) return false;
+        it->second -= Y;
+        return true;
     }
 
-    bool CREDIT(string x, long long y) {
-        if (!mp.count(x)) return false;
-        mp[x] += y; return true;
+    bool CREDIT(string X, long long Y) {
+        auto it = mp.find(X);
+        if (it == mp.end()) return false;
+        it->second += Y;
+        return true;
     }
 
-    long long BALANCE(string x) {
-        return mp.count(x) ? mp[x] : -1;
+    long long BALANCE(string X) {
+        auto it = mp.find(X);
+        return it == mp.end() ? -1 : it->second;
     }
 };
 
 int main() {
-    int q; cin >> q;
-    Bank b;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    while (q--) {
-        string t, x; cin >> t >> x;
+    int Q; cin >> Q;
+    cin.ignore();
+    Bank bank;
 
-        if (t == "BALANCE") cout << b.BALANCE(x) << "\n";
-        else {
-            long long y; cin >> y;
-            if (t == "CREATE") cout << (b.CREATE(x,y)?"true":"false") << "\n";
-            else if (t == "DEBIT") cout << (b.DEBIT(x,y)?"true":"false") << "\n";
-            else cout << (b.CREDIT(x,y)?"true":"false") << "\n";
+    while (Q--) {
+        string type, X;
+        cin >> type >> X;
+        if (type == "BALANCE") {
+            cout << bank.BALANCE(X) << "\n";
+        } else {
+            long long Y; cin >> Y;
+            bool res;
+            if      (type == "CREATE") res = bank.CREATE(X, Y);
+            else if (type == "DEBIT")  res = bank.DEBIT(X, Y);
+            else                       res = bank.CREDIT(X, Y);
+            cout << (res ? "true" : "false") << "\n";
         }
     }
 }
